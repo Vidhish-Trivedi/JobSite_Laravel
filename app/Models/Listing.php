37ba99a1912a4d6ca7082a0058c07ba06a6faca0
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-04-01 16:04:32 +0530

    Eloquent ORM for Listing Model, database seeding

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,39 +1,11 @@
 <?php
-    namespace App\Models;
 
-    class Listing {
-        public static function all() {
-            return(
-                [
-                    [
-                        "id" => 1,
-                        "title" => "Listing One",
-                        "description" => "This is the description for Listing One",
-                    ],
-                    [
-                        "id" => 2,
-                        "title" => "Listing Two",
-                        "description" => "This is the description for Listing Two",
-                    ],
-                    [
-                        "id" => 3,
-                        "title" => "Listing Three",
-                        "description" => "This is the description for Listing Three",
-                    ]
-                ]
-            );
-        }
-    
-        public static function find($id) {
-            $listings = self::all();
+namespace App\Models;
 
-            foreach ($listings as $el) {
-                if($el["id"] == $id) {
-                    return($el);
-                }
-            }
-        }
-    
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-?>
+class Listing extends Model
+{
+    use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Listing;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Listing::create([
+            "title" => "Title 1",
+            "tags" => "Hello, Title, Tag",
+            "companyName" => "yourCompany",
+            "location" => "Boston, MA",
+            "email" => "em1@email.com",
+            "website" => "h t t p s : //",
+            "description" => "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+        ]);
+        
+        Listing::create([
+            "title" => "Title 2",
+            "tags" => "JS, HTML, CSS",
+            "companyName" => "your_Company",
+            "location" => "Brooklyn, NY",
+            "email" => "em2@email.com",
+            "website" => "h t t p s : //",
+            "description" => "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+        ]);
     }
 }
