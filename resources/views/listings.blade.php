14731adbbfcb0251e7d1250e879e5a45e6b9e019
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-04-02 11:33:54 +0530

    Layout using blade templates and partials

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,14 +1,50 @@
-{{-- Blade avoids php tags for variables --}}
-<h1>{{ $heading }}</h1>
+@extends('layout')
 
-{{-- Blade has directives,'@' for conditionals and loops, etc... --}}
-@if(count($listings) === 0)
-    <p>No Listings Found</p>
-@endif
+@section('content')
+@include('partials._hero')
+@include('partials._search')
+    {{-- Blade avoids php tags for variables --}}
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
-@foreach($listings as $el)
-    <h2>
-        <a href="/listings/{{ $el['id'] }}">{{ $el["title"] }}</a>
-    </h2>
-    <p>{{ $el["description"] }}</p>
-@endforeach
+        {{-- Blade has directives,'@' for conditionals and loops, etc... --}}
+        @if (count($listings) === 0)
+            <p>No Listings Found</p>
+        @endif
+
+        @foreach ($listings as $listing)
+            <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                <div class="flex">
+                    <img class="hidden w-48 mr-6 md:block" src="{{asset('images/no-image.png')}}" alt="" />
+                    <div>
+                        <h3 class="text-2xl">
+                            <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
+                        </h3>
+                        <div class="text-xl font-bold mb-4">{{$listing->companyName}}</div>
+                        <ul class="flex">
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                <a href="#">Laravel</a>
+                            </li>
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                <a href="#">API</a>
+                            </li>
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                <a href="#">Backend</a>
+                            </li>
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                <a href="#">Vue</a>
+                            </li>
+                        </ul>
+                        <div class="text-lg mt-4">
+                            <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+
+    </div>
+@endsection
