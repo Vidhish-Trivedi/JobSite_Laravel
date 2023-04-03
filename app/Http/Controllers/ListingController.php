38a1d6a7128e4dd831d6726ae9604000b6536935
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-04-03 23:29:02 +0530

    Filter using tags

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,8 +9,13 @@ class ListingController extends Controller
 {
     // Get all listings.
     public function index() {
-        return view('listings/index', [                          // can pass also use "listings.index", see next method in class.
-            "listings" => Listing::all()
+        // dd($request);        // need parameter Request $request
+        // Can also use the request() helper function.
+        // dd(request()->tag);
+        // dd(request('tag'));
+
+        return view('listings/index', [                          // Can also use "listings.index", see next method in class.
+            "listings" => Listing::latest()->filter(request(['tag']))->get()              // Sorted, filter using scopeFilter in ListingController.
         ]);
     }
 
